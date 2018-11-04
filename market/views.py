30fe68aa474e4adc9ae40f3980759364bbbbd5e1
ottovbismarck <fritzj96@gmail.com>
2018-11-04 01:17:42 -0700

    Added some test functionality

--- market/views.py
+++ market/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
   return HttpResponse("Hello World!")
+def addJohn(request):
+  user = User.objects.create_user('John', 'john@harrisranch.com', 'JohnsPassword', first_name="John", last_name="Harris")
+  user.save()
+  return HttpResponse("Added")
