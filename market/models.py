34cefa40828aa75b656ef8ac39519e3d4c24849a
ottovbismarck <fritzj96@gmail.com>
2018-11-04 01:27:57 -0800

    fixed a field on the listing model & added to the template

--- market/models.py
+++ market/models.py
@@ -6,7 +6,7 @@ class Listing(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   listing_datetime = models.DateTimeField(auto_now_add=True)
   af_available = models.IntegerField()
-  price_per_af = models.IntegerField
+  price_per_af = models.IntegerField(default=0)
   water_district = models.CharField(max_length=50)
   def __str__(self):
     return (self.user.username + ' ' + str(self.listing_datetime)[:16])
--- market/views.py
+++ market/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.models import User
+from .models import Listing
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello World!")
-def addJohn(request):
-  user = User.objects.create_user('John', 'john@harrisranch.com', 'JohnsPassword', first_name="John", last_name="Harris")
-  user.save()
-  return HttpResponse("Added")
+  context = {}
+  context["last_ten_listings"] = Listing.objects.order_by('-listing_datetime')[:10] 
+  return render(request, 'market/index.html', context)
